86b951bad8995490804739fe22a8ce8b500b9e40
faizanayubi <indianayubi@gmail.com>
2015-09-06 19:21:53 +0530

    Initaialized SEO

--- application/plugins/seo/initialize.php
+++ application/plugins/seo/initialize.php
@@ -5,11 +5,11 @@ include("seo.php");
 
 $seo = new SEO(array(
     "title" => "EarnBugs Affiliate Network",
-    "keywords" => "arn money, facebook page monetization" ,
+    "keywords" => "arn money, facebook page monetization",
     "description" => "Monetize your platform through us, get paid with high cpm",
-    "author" => "https://plus.google.com/107837531266258418226",
+    "author" => "",
     "robots" => "INDEX,FOLLOW",
-    "photo" => CDN . "images/logo.png"
+    "photo" => CDN . "img/logo.png"
 ));
 
 Framework\Registry::set("seo", $seo);

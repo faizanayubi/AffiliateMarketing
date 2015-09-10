b911c09922c2bed0259435851169b437076109a9
faizanayubi <indianayubi@gmail.com>
2015-09-10 19:58:16 +0530

    Social Model Added

---
+++ application/models/social.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Description of social
+ *
+ * @author Faizan Ayubi
+ */
+class Social extends Shared\Model {
+    /**
+     * @column
+     * @readwrite
+     * @type integer
+     */
+    protected $_user_id;
+    
+    /**
+     * @column
+     * @readwrite
+     * @type text
+     * @length 64
+     */
+    protected $_platform;
+    
+    /**
+     * @column
+     * @readwrite
+     * @type text
+     */
+    protected $_link;
+}

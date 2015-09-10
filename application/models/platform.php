a3ee57576ace76218275cecff779ebc5fe6ef38a
faizanayubi <indianayubi@gmail.com>
2015-09-10 19:12:47 +0530

    Added Models

---
+++ application/models/platform.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Description of platform
+ *
+ * @author Faizan Ayubi
+ */
+class Platform extends Shared\Model{
+    
+    /**
+     * @column
+     * @readwrite
+     * @type text
+     * @length 100
+     * 
+     * @validate required, min(3), max(32)
+     * @label name
+     */
+    protected $_name;
+    
+    /**
+     * @column
+     * @readwrite
+     * @type text
+     * 
+     * @validate required, alpha, min(3), max(32)
+     * @label link
+     */
+    protected $_link;
+}

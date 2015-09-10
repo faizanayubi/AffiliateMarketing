2975323be705715e34af65ee5ed9b21a2563ec35
faizanayubi <indianayubi@gmail.com>
2015-09-10 19:15:59 +0530

    SEO tags added

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -10,15 +10,21 @@ use Framework\Controller as Controller;
 class Home extends Controller {
 
     public function index() {
-        
+        $this->getLayoutView()->set("seo", Framework\Registry::get("seo"));
     }
     
     public function privacy() {
-        
+        $this->seo(array(
+            "title" => "Privacy Policy",
+            "view" => $this->getLayoutView()
+        ));
     }
     
     public function termsofuse() {
-        
+        $this->seo(array(
+            "title" => "Terms of Use",
+            "view" => $this->getLayoutView()
+        ));
     }
 
 }

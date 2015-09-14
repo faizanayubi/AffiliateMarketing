24cb2948c575c7ba3406771c0a219a3bef655ad6
faizanayubi <indianayubi@gmail.com>
2015-09-14 14:23:01 +0530

    Earning Model Created

---
+++ application/models/earning.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Description of earning
+ *
+ * @author Faizan Ayubi
+ */
+class Earning extends \Shared\Model {
+    /**
+     * @column
+     * @readwrite
+     * @type integer
+     */
+    protected $_item_id;
+    
+    /**
+     * @column
+     * @readwrite
+     * @type text
+     * @length 255
+     */
+    protected $_amount;
+    
+    /**
+     * @column
+     * @readwrite
+     * @type integer
+     */
+    protected $_user_id;
+    
+    /**
+     * @column
+     * @readwrite
+     * @type integer
+     */
+    protected $_stat_id;
+}

67702d1e2e04389463460a052a11c7be1100321f
faizanayubi <indianayubi@gmail.com>
2015-09-14 14:23:22 +0530

    Analytics Model Googl Created

---
+++ application/models/stat.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * Description of stat
+ *
+ * @author Faizan Ayubi
+ */
+class Stat extends Shared\Model {
+    /**
+     * @column
+     * @readwrite
+     * @type integer
+     */
+    protected $_link_id;
+    
+    /**
+     * @column
+     * @readwrite
+     * @type integer
+     */
+    protected $_shortUrlClicks;
+    
+    /**
+     * @column
+     * @readwrite
+     * @type integer
+     */
+    protected $_longUrlClicks;
+    
+    /**
+     * @column
+     * @readwrite
+     * @type text
+     * @length 255
+     */
+    protected $_referrers;
+    
+    /**
+     * @column
+     * @readwrite
+     * @type text
+     */
+    protected $_countries;
+    
+    /**
+     * @column
+     * @readwrite
+     * @type text
+     */
+    protected $_browsers;
+    
+    /**
+     * @column
+     * @readwrite
+     * @type text
+     */
+    protected $_platforms;
+}

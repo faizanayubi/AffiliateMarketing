e431eb8627d5884387e66209f83430340c001e7d
faizanayubi <indianayubi@gmail.com>
2015-09-18 22:27:55 +0530

    Added account model

---
+++ application/models/account.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * Description of bank
+ *
+ * @author Faizan Ayubi
+ */
+class Account extends \Shared\Model {
+    
+    /**
+     * @column
+     * @readwrite
+     * @type integer
+     */
+    protected $_user_id;
+    
+    /**
+     * @column
+     * @readwrite
+     * @type text
+     * @length 255
+     * 
+     * @validate required, min(3), max(255)
+     * @label holder name
+     */
+    protected $_name;
+    
+    /**
+     * @column
+     * @readwrite
+     * @type text
+     * @length 100
+     * 
+     * @validate required, min(3), max(32)
+     * @label bank name
+     */
+    protected $_bank;
+    
+    /**
+     * @column
+     * @readwrite
+     * @type text
+     * @length 100
+     * 
+     * @validate required, min(3), max(32)
+     * @label account number
+     */
+    protected $_number;
+    
+    /**
+     * @column
+     * @readwrite
+     * @type text
+     * @length 100
+     * 
+     * @validate required, min(3), max(32)
+     * @label ifsc code
+     */
+    protected $_ifsc;
+}

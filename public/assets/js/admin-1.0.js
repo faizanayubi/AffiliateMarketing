b6ef2a86fc5d3eef026085797e4234279829cbff
faizanayubi <indianayubi@gmail.com>
2015-09-18 20:09:07 +0530

    Added Data Analysis

--- public/assets/js/admin-1.0.js
+++ public/assets/js/admin-1.0.js
@@ -2,6 +2,12 @@ $(function () {
     $('#side-menu').metisMenu();
 });
 
+$(function () {
+    $('select[value]').each(function () {
+        $(this).val(this.getAttribute("value"));
+    });
+});
+
 //Loads the correct sidebar on window load,
 //collapses the sidebar on window resize.
 // Sets the min-height of #page-wrapper to window size
@@ -33,3 +39,76 @@ $(function () {
         element.addClass('active');
     }
 });
+
+$(document).ready(function () {
+
+    //initialize beautiful datetime picker
+    $("input[type=date]").datepicker();
+    $("input[type=date]").datepicker("option", "dateFormat", "yy-mm-dd");
+
+    $('#created_stats').submit(function (e) {
+        $('#stats').html('<p class="text-center"><i class="fa fa-spinner fa-spin fa-5x"></i></p>');
+        e.preventDefault();
+        var data = $(this).serializeArray();
+        request.read({
+            action: "admin/data",
+            data: data,
+            callback: function (data) {
+                $('#stats').html('');
+                if (data.data) {
+                    Morris.Bar({
+                        element: 'stats',
+                        data: toArray(data.data),
+                        xkey: 'y',
+                        ykeys: ['a'],
+                        labels: ['Total']
+                    });
+                }
+            }
+        });
+    });
+    
+    $('button[name=message]').click(function (e) {
+        var self = this;
+        window.opts.subject = $(this).data("subject");
+        window.opts.email = $(this).data("from");
+        $('#message_modal').modal('show');
+    });
+
+    $('#messageform').submit(function(e) {
+        e.preventDefault();
+        var body = $('#body').val();
+        request.create({
+            action: "employer/messages",
+            data: {action: 'support', subject: window.opts.subject, email: window.opts.email, body: body},
+            callback: function (data) {
+                $('#status').html('Message Sent Successfully!!!');
+                $('#message_modal').modal('hide');
+            }
+        });
+    });
+    
+    // find all the selectors 
+    var types = $('#addOptions select');
+    types.on("change", function (){ // bind the change function
+        var value = $(this).val();
+
+        // if text box is selected then show it and hide the file upload or vice-versa
+        if (value === "text") {
+            $("#type").find("input[type='text']").toggleClass("hide").attr("required", "");
+            $("#type").find("input[type='file']").toggleClass("hide");
+        } else if (value === "image") {
+            $("#type").find("input[type='file']").toggleClass("hide");
+            $("#type").find("input[type='text']").toggleClass("hide").removeAttr("required");
+        }
+    });
+    
+
+});
+
+function toArray(object) {
+    var array = $.map(object, function (value, index) {
+        return [value];
+    });
+    return array;
+}

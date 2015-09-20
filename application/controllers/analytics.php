c26ee3903958f4ba583e639ece711a806f0107dc
faizanayubi <indianayubi@gmail.com>
2015-09-20 12:20:15 +0530

    Completed Full shortURL Analytics

--- application/controllers/analytics.php
+++ application/controllers/analytics.php
@@ -5,14 +5,26 @@
  *
  * @author Faizan Ayubi
  */
+use Framework\Registry as Registry;
+use Framework\RequestMethods as RequestMethods;
+
 class Analytics extends Admin {
     
     /**
      * @before _secure, changeLayout
      */
-    public function content($id = NULL) {
-        $this->seo(array("title" => "Delete Content", "view" => $this->getLayoutView()));
+    public function full() {
+        $this->seo(array("title" => "shortURL Analytics", "view" => $this->getLayoutView()));
         $view = $this->getActionView();
+        if (RequestMethods::get("shortURL")) {
+            $shortURL = RequestMethods::get("shortURL");
+            $googl = Registry::get("googl");
+            $object = $googl->analyticsFull($shortURL);
+            //echo '<pre>', print_r($object), '</pre>';
+            
+            $view->set("shortURL", $shortURL);
+            $view->set("googl", $object);
+        }
     }
     
 }

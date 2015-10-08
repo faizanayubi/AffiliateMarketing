1e05bcf8baa61d2a876afce252bcc65e4cfbff11
Faizan Ayubi <indianayubi@gmail.com>
2015-10-08 12:19:22 +0530

    Contact Controller created

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -6,6 +6,7 @@
  * @author Faizan Ayubi
  */
 use Framework\Controller as Controller;
+use Framework\RequestMethods as RequestMethods;
 
 class Home extends Controller {
 
@@ -27,4 +28,24 @@ class Home extends Controller {
         ));
     }
 
+    public function contact() {
+        $this->seo(array(
+            "title" => "Contact Us",
+            "view" => $this->getLayoutView()
+        ));
+
+        if (RequestMethods::post("message")) {
+            $emails = array();
+            array_push($emails, RequestMethods::post("email"));
+            $options = array(
+                "template" => "blank",
+                "subject" => RequestMethods::post("subject"),
+                "message" => RequestMethods::post("message"),
+                "emails" => $emails,
+                "delivery" => "mailgun"
+            );
+            $this->notify($options);
+            $view->set("success", TRUE);
+        }
+    }
 }

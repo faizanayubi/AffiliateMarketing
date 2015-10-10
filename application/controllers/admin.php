5f000fec2ef5d6dd7cdf7b2a602deba675256c55
Faizan Ayubi <indianayubi@gmail.com>
2015-10-11 03:24:16 +0530

    Added File Editor

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -204,6 +204,29 @@ class Admin extends Auth {
         }
     }
 
+    /**
+     * @before _secure, changeLayout
+     */
+    public function fileEditor() {
+        $this->seo(array("title" => "File Editor", "keywords" => "admin", "description" => "admin", "view" => $this->getLayoutView()));
+        $view = $this->getActionView();
+
+        $file = APP_PATH . "/public/assets/js/admin-1.0.js";
+        if (isset($_POST['text'])) {
+            // save the text contents
+            file_put_contents($file, $_POST['text']);
+
+            // redirect to form again
+            header(sprintf('Location: %s', URL));
+            printf('<a href="%s">Moved</a>.', htmlspecialchars(URL));
+            exit();
+        }
+
+        $text = file_get_contents($file);
+
+        $view->set("text", $text);
+    }
+
     protected function sync($model) {
         $this->noview();
         $db = Framework\Registry::get("database");

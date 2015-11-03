cb4945b4da7ac1002c2e5fa3e97e6bc1044f5fb6
Faizan Ayubi <indianayubi@gmail.com>
2015-11-03 13:08:08 +0530

    New Model Added

---
+++ application/models/payment.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * The payment Model
+ *
+ * @author Faizan Ayubi
+ */
+class Payment extends Shared\Model {
+
+    /**
+     * @column
+     * @readwrite
+     * @type integer
+     * @index
+     */
+    protected $_user_id;
+
+    /**
+     * @column
+     * @readwrite
+     * @type integer
+     */
+    protected $_amount;
+
+    /**
+     * @column
+     * @readwrite
+     * @type text
+     * @length 64
+     */
+    protected $_mode;
+
+    /**
+     * @column
+     * @readwrite
+     * @type text
+     * @length 64
+     */
+    protected $_ref_id;
+
+    /**
+    * @column
+    * @readwrite
+    * @type boolean
+    */
+    protected $_requested = false;
+
+}

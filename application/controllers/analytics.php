53c2c5db78b50239e5ee829002429cc6d480c681
Faizan Ayubi <indianayubi@gmail.com>
2015-12-02 14:22:43 +0530

    Log Manager Created

--- application/controllers/analytics.php
+++ application/controllers/analytics.php
@@ -10,6 +10,26 @@ use Framework\RequestMethods as RequestMethods;
 use \Curl\Curl;
 
 class Analytics extends Admin {
+
+    /**
+     * @before _secure, changeLayout, _admin
+     */
+    public function logs() {
+        $this->seo(array("title" => "Activity Logs", "view" => $this->getLayoutView()));
+        $view = $this->getActionView();
+
+        $logs = array();
+        $path = APP_PATH . "/logs";
+        $iterator = new DirectoryIterator($path);
+
+        foreach ($iterator as $item) {
+            if (!$item->isDot()) {
+                array_push($logs, $item->getFilename());
+            }
+        }
+
+        $view->set("logs", $logs);
+    }
     
     /**
      * @before _secure, changeLayout, _admin
